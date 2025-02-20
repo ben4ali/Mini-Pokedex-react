5feeab478a8a7f97e0174498d3a8ddf691659b9b
Ali Benkarrouch <2268340@crosemont.qc.ca>
2025-02-20 11:50:47 -0500

    Commit initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { PokemonDetails } from './components/PokemonDetails';
+import useFetch from './hooks/usefetch';
+import { useState } from 'react';
 
 function App() {
+  const [nom, setNom] = useState('');
+  const { data } = useFetch('https://pokeapi.co/api/v2/pokemon/'+nom);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='pokedex-content'>
+          {data && <PokemonDetails data={data} />}
+          <div className='pokedexForm'>
+
+            <div className='decoration'>
+              <div className='markModel'>
+                <p> ABENKARROUCH </p>
+              </div>
+              <div className='sensors'>
+                <div className='Camera'></div>
+                <div className='lightBtns'>
+                  <span></span>
+                  <span></span>
+                </div>
+            </div>
+              </div>
+
+            <form>
+              <label>Numéro ou nom du pokémon</label>
+              <div className='input-group'>
+                <input onChange={(e) => setNom(e.target.value.toLowerCase())} type='text' placeholder='Chercher un pokémon' />
+              </div>
+            </form>
+            <div className='errorHolder'>
+              <p>Nom ou Numéro</p>
+              <p>{nom}</p>
+            </div>
+
+          </div>
+      </div>
     </div>
   );
 }
